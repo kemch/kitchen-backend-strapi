0a26275955e49ab86d3867acf3d41e6389b5851b
Kurt Emch <kurtemch@gmail.com>
2021-02-28 17:24:05 -0500

    supports a slug in ingredients component list for visible entry name in the actual recipe

--- api/recipe/models/recipe.js
+++ api/recipe/models/recipe.js
@@ -5,4 +5,45 @@
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Triggered before user creation.
+   */
+  lifecycles: {
+    async beforeCreate(data) {
+      for (var i = data.IngredientList.length - 1; i >= 0; i--) {
+        const ing_id = data.IngredientList[i].ingredient;
+        const res = await strapi.services.ingredient.find({ id: ing_id });
+        const name = res[0].Name;
+        const qty= data.IngredientList[i].Quantity;
+        const unit= data.IngredientList[i].Unit;
+        let prep = '';
+        if (data.IngredientList[i].Preparation != null) {
+          prep = data.IngredientList[i].Preparation;
+        }
+
+        const line = `${qty} ${unit} ${name} ${prep}`;
+        data.IngredientList[i].slug = line;
+      }
+    },
+    async beforeUpdate(params, data) {
+      
+      if (data.IngredientList) {
+        for (var i = data.IngredientList.length - 1; i >= 0; i--) {
+          const ing_id = data.IngredientList[i].ingredient;
+          const res = await strapi.services.ingredient.find({ id: ing_id });
+          const name = res[0].Name;
+          const qty= data.IngredientList[i].Quantity;
+          const unit= data.IngredientList[i].Unit;
+          let prep = '';
+          if (data.IngredientList[i].Preparation != null) {
+            prep = data.IngredientList[i].Preparation;
+          }
+
+          const line = `${qty} ${unit} ${name} ${prep}`;
+          data.IngredientList[i].slug = line;
+        }
+      }
+    },
+  },
+};
